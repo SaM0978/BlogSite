bff38206f9347015e4e15ed6f844cabb16312c65
Abdus Samad <cricker091@gmail.com>
2024-01-05 11:50:19 +0530

    blogFrontend

--- src/components/Blog/Blog.tsx
+++ src/components/Blog/Blog.tsx
@@ -4,26 +4,30 @@ import "../../styles/global.css";
 
 interface BlogProps {
   blogObject: {
-    title: Object;
+    title: string;
     description: string;
     imageUrl: string;
   };
 }
 
-const Blog: React.FC<BlogProps> = (props) => {
-  const { title, description, imageUrl } = props.blogObject;
-  console.log(title);
+const Blog: React.FC<BlogProps> = ({ blogObject }) => {
+  const { title, description, imageUrl } = blogObject;
+
   return (
     <div className="Blog b-r">
       <div className="blog-img d-flex b-r">
-        <img src={imageUrl} className="blog-thumbnail b-r" />
+        <img
+          src={imageUrl}
+          className="blog-thumbnail b-r"
+          alt="Blog Thumbnail"
+        />
       </div>
       <div className="blog-body">
-        <div className="blog-title d-flex">
-          <h2 className=" title">{title}</h2>
+        <div className="blog-title">
+          <h2 className="title t-center">{title}</h2>
         </div>
         <div className="blog-description">
-          <h3 className="t-center description">{description} dsd</h3>
+          <h3 className="description">{description}..</h3>
         </div>
       </div>
     </div>
--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,29 +1,66 @@
 import "./Home.css";
 import Blog from "../Blog/Blog";
 
-function validateTitle(blogTitle: string): {
-  truncatedTitle: string;
-  titleArray: string[];
-} {
-  let truncatedTitle: string = "";
-  let titleArray: string[] = [];
+function validateTitle(blogTitle: string) {
+  const MAX_LENGTH = 29;
+  console.log(blogTitle.length);
 
-  if (blogTitle.split(" ").length <= 5) {
-    truncatedTitle = blogTitle;
-    titleArray = blogTitle.split(" ");
+  if (blogTitle.length > MAX_LENGTH) {
+    const truncatedTitle = blogTitle.slice(0, MAX_LENGTH);
+    return truncatedTitle;
   } else {
-    titleArray = blogTitle.split(" ");
-    truncatedTitle =
-      titleArray.slice(0, 5).join(" ") + "\n" + titleArray.slice(5).join(" ");
+    return blogTitle;
   }
+}
 
-  return { truncatedTitle, titleArray };
+function descriptionShorter(description: string) {
+  const MAX_LENGTH = 46;
+  console.log(description.length);
+
+  if (description.length > MAX_LENGTH) {
+    const truncatedDescription = description.slice(0, MAX_LENGTH);
+    return truncatedDescription;
+  } else {
+    return description;
+  }
 }
 
 const blogs = [
   {
     title: validateTitle("Reason Behind Next Js's Rapid Growth"),
-    description: "description",
+    description: descriptionShorter(
+      "Next.js React Features With File-Based Routing With Many Things"
+    ),
+    imageUrl:
+      "https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg",
+  },
+  {
+    title: validateTitle("Reason Behind Javascript Js's Rapid Decline"),
+    description: descriptionShorter("Sun tzu"),
+    imageUrl:
+      "https://bairesdev.mo.cloudinary.net/blog/2023/08/What-Is-JavaScript-Used-For.jpg",
+  },
+  {
+    title: validateTitle("Reason Behind Next Js's Rapid Growth"),
+    description: descriptionShorter(
+      "Next.js React Features With File-Based Routing With Many Things"
+    ),
+    imageUrl:
+      "https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg",
+  },
+  {
+    title: validateTitle("Reason Behind Next Js's Rapid Growth"),
+    description: descriptionShorter(
+      "Next.js React Features With File-Based Routing With Many Things"
+    ),
+    imageUrl:
+      "https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg",
+  },
+  {
+    title: validateTitle("Reason Behind Next Js's Rapid Growth"),
+    description: descriptionShorter(
+      "Next.js React Features With File-Based Routing With Many Things"
+    ),
     imageUrl:
       "https://miro.medium.com/v2/resize:fit:1000/1*v3XndYeIsBtk4CkpMf7vmA.jpeg",
   },
@@ -32,9 +69,9 @@ const blogs = [
 export default function Home() {
   return (
     <main>
-      <div className="row bs" style={{ marginTop: "10px", marginLeft: "10px" }}>
+      <div className="" style={{ marginTop: "10px", marginLeft: "10px" }}>
         {blogs.map((blog, index) => (
-          <div className="col" key={index}>
+          <div className="flexing" key={index}>
             <Blog blogObject={blog} />
           </div>
         ))}
